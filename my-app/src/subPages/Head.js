6ecc88ba451470c85548b86efbbfeacc7ff62292
Abdullilah Alhaj Younes <abdullilah.alhaj.younes@oracle.com>
2017-12-13 18:22:47 +0100

    onChange method change the content

--- my-app/src/subPages/Head.js
+++ my-app/src/subPages/Head.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class Head extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            name: 'Home'
+        }
+    }
+
+    changeLinkName(e){
+        this.setState({
+            name: e.target.value
+        });
+    }
+
     render() {
         return (
             <div className="Head">
                 <nav>
                     <ul className='nav-list'>
-                        <li><a href=''>Home</a></li>
+                        <li><a href=''>{this.state.name}</a></li>
                         <li><a href=''>About</a></li>
                     </ul>
+                    <input type='text' value={this.state.name} onChange={this.changeLinkName.bind(this)} />
                 </nav>
             </div>
         );
